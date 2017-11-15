ec329d3b28d13b326e86632e11cf43e33b2b49e3
SequinYF <catherine199787@outlook.com>
2017-11-15 20:05:07 +0800

    os

---
+++ cs/thread.c
@@ -0,0 +1,11 @@
+/*  POSIX 下线程控制的实验程序残缺版 */ 
+#include <stdio.h> 
+#include <sys/types.h>
+#include <unistd.h> 
+#include <ctype.h>
+#include <pthread.h> 
+
+#define MAX_THREAD 3 /* 线程的个数 */
+unsigned long long main_counter, counter[MAX_THREAD]; 
+/* unsigned long  long是比long还长的整数 */
+void* thread_worker(void*); 
