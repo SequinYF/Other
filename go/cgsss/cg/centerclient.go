3017b42f24eacdb6b313d4f71cf4db0df9e2e825
SequinYF <catherine199787@outlook.com>
2017-11-26 13:58:02 +0800

    Casual Game

---
+++ go/cgsss/cg/centerclient.go
@@ -0,0 +1,57 @@
+package cg
+
+import (
+	"cgsss/ipc"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type CenterClient struct {
+	*ipc.IpcClient
+}
+
+func (client *CenterClient) AddPlayer(palyer * Player) error{
+	b, err := json.Marshal(*palyer)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Call("addplayer", string(b))
+	if err != nil || resp.Code == "200" {
+		return nil
+	}
+	return err
+}
+
+func (client *CenterClient) RemovePlayer(name string) error {
+	ret, _ := client.Call("removeplayer", name)
+	if ret.Code == "200" {
+		return nil
+	}
+	return errors.New(ret.Code)
+}
+
+func (client *CenterClient) ListPlayer(params string) (ps [] *Player, err error) {
+	resp, _:= client.Call("listplayer", params)
+	if resp.Code != "200" {
+		err = errors.New(resp.Code)
+		return
+	}
+
+	err = json.Unmarshal([]byte(resp.Body), &ps)
+	return
+}
+
+func (client *CenterClient) Broadcast(message string) error {
+	m := &Message{Content:message}
+	b, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	resp, _ := client.Call("broadcast", string(b))
+	if resp.Code == "200" {
+		return nil
+	}
+	return errors.New(resp.Code)
+}
